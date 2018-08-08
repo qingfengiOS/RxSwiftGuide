1e4e51bee446189be0614822cf74217463c885d4
情风 <291753906@qq.com>
2018-08-08 18:03:31 +0800

    update

--- Podfile
+++ Podfile
@@ -6,6 +6,7 @@ target 'RxSwiftGuide' do
   use_frameworks!
 pod 'RxSwift', '~> 4.0'
 pod 'RxCocoa', '~> 4.0'
+pod 'Moya', '~> 11.0.2'
   # Pods for RxSwiftGuide
 
   target 'RxSwiftGuideTests' do
--- RxSwiftGuide/ViewController.swift
+++ RxSwiftGuide/ViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Moya
 
 class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
     let kCellIdentifier = "cellIdentifier"
     @IBOutlet weak var tableView: UITableView!
     var dataArray: [String] = []
+    
+    let disposeBag = DisposeBag()
     
     //MARK:-View Life Cycle
     override func viewDidLoad() {
@@ -32,6 +37,9 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
                     ]
         self.tableView.tableFooterView = UIView()
         self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: kCellIdentifier)
+        
+        rxKeywords()
+        networkWithMoya()
     }
 
     //MARK:-TableViewDelegate/DataSource
@@ -66,5 +74,168 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
         return resViewController
     }
 
+    //MARK:- RxSwift关键字
+    func rxKeywords() {
+        never()
+        empty()
+        just()
+        of()
+        from()
+        create()
+        range()
+        repeatElement()
+    }
+    
+    func never() {
+        let neverSequence = Observable<String>.never()
+        neverSequence
+            .subscribe{ (_) in
+                print("this will never be printed")
+            }.disposed(by: disposeBag)
+        
+        print("-----------------")
+    }
+    
+    func empty() {
+        Observable<String>
+            .empty()
+            .subscribe{ event in
+                print(event)
+            }.disposed(by: disposeBag)
+        
+        print("-----------------")
+    }
+    
+    func just() {
+        Observable.just("🍎")
+            .subscribe{ event in
+                print(event)
+            }.disposed(by: disposeBag)
+        
+        print("-----------------")
+    }
+    
+    func of() {
+        //of是创建一个sequence能发出很多种事件信号
+        Observable.of("🐈","🐩","🐲")
+            .subscribe(onNext: { element in
+                print(element)
+            }).disposed(by: disposeBag)
+        //        如果把上面的onNext:去掉的话，结果会是这样子，也正好对应了我们subscribe中，subscribe只监听事件。
+        print("如果把上面的onNext:去掉的话，结果会是这样子，也正好对应了我们subscribe中，subscribe只监听事件")
+        Observable.of("🐈","🐩","🐲")
+            .subscribe({ element in
+                print(element)
+            }).disposed(by: disposeBag)
+        
+        print("-----------------")
+    }
+    
+    func from() {
+        Observable.from(["🍐", "🍌", "🍊"])
+            .subscribe(onNext:{ element in
+                print(element)
+            }).disposed(by: disposeBag)
+    }
+    
+    
+    /// create操作符传入一个观察者observer，然后调用observer的onNext，onCompleted和onError方法。返回一个可观察的obserable序列。
+    func create() {
+        let myJust = { (element: String) -> Observable<String> in
+            return Observable.create{ observer in
+                observer.on(.next(element))
+                observer.on(.completed)
+                return Disposables.create()
+            }
+        }
+        
+        myJust("🔴")
+            .subscribe{ print($0) }
+            .disposed(by: disposeBag)
+        print("-----------------")
+    }
+    
+    
+    /// range就是创建一个sequence，他会发出这个范围中的从开始到结束的所有事件
+    func range() {
+        Observable.range(start: 1, count: 3)
+            .subscribe(onNext: {
+                print($0)
+            })
+            .disposed(by: disposeBag)
+        print("-----------------")
+    }
+    
+    
+    /// 创建一个sequence，发出特定的事件n次
+    func repeatElement() {
+        Observable.repeatElement("A")
+            .take(2)
+            .subscribe(onNext: { print($0) })
+            .disposed(by: disposeBag)
+        
+        print("-----------------")
+    }
+    
+    
+    func networkWithMoya() {
+        let provider = MoyaProvider<MyAPI>()
+        provider.request(.Show) { (result) in
+            print(result)
+        }
+    }
+    
 }
 
+
+enum MyAPI {
+    case Show
+    case Create(title: String, body: String, userId: Int)
+}
+
+extension MyAPI: TargetType {
+    var baseURL: URL {
+        return URL(string: "http://jsonplaceholder.typicode.com")!
+    }
+    var path: String {
+        switch self {
+        case .Show:
+            return "/posts"
+        case .Create(title: _, body: _, userId: _):
+            return "/posts"
+        }
+    }
+    var method: Moya.Method {
+        switch self {
+        case .Show:
+            return .get
+        case .Create(title: _, body: _, userId: _):
+            return .post
+        }
+    }
+    var paramter: [String: Any]? {
+        
+        switch self {
+        case .Show:
+            return nil
+        case .Create(let title, let body, let userId):
+            return ["title": title, "body": body, "userId": userId]
+        }
+    }
+    var sampleData: Data {
+        switch self {
+        case .Show:
+            return "[]".data(using: String.Encoding.utf8)!
+        case .Create(_, _, _):
+            return "Create post successfully".data(using: String.Encoding.utf8)!
+        }
+    }
+    var task: Task {
+        return .requestPlain
+    }
+    
+    var headers: [String : String]? {
+        return nil
+    }
+    
+}
